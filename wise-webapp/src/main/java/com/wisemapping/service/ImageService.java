e7485850d2b8f719904ecb7ea102842077c13776
Mariela Michalek <mariela.michalek@gmail.com>
2014-09-07 20:18:04 -0300

    add ImageService

---
+++ wise-webapp/src/main/java/com/wisemapping/service/ImageService.java
@@ -0,0 +1,20 @@
+package com.wisemapping.service;
+
+import com.wisemapping.exceptions.WiseMappingException;
+import com.wisemapping.model.Image;
+import com.wisemapping.model.User;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.List;
+
+public interface ImageService {
+
+    void addImage(@NotNull final Image image) throws WiseMappingException;
+
+    @NotNull List<Image> getAll(@NotNull final User user);
+
+    @Nullable
+    Image getImageById(int id);
+
+}
