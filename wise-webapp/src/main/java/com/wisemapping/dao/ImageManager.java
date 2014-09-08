f78511acca855e586e271d3c30757fb1cad3c37d
Mariela Michalek <mariela.michalek@gmail.com>
2014-09-07 21:26:39 -0300

    add ImageManager

---
+++ wise-webapp/src/main/java/com/wisemapping/dao/ImageManager.java
@@ -0,0 +1,21 @@
+package com.wisemapping.dao;
+
+import com.wisemapping.model.Image;
+import com.wisemapping.model.User;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.List;
+
+public interface ImageManager {
+
+    void addImage(@NotNull final Image image);
+
+    void saveImage(@NotNull final Image image);
+
+    @NotNull
+    List<Image> getAllImages(@NotNull final User user);
+
+    @Nullable
+    Image getImageById(int id);
+}
