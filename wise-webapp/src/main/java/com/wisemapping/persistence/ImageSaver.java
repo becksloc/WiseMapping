f2a10dee47a64cfd9b215ebfed3308559dc575d5
Ezequiel Bergamaschi <ezequielbergamaschi@gmail.com>
2014-09-08 12:51:21 -0300

    image saver

--- wise-webapp/src/main/java/com/wisemapping/persistence/ImageSaver.java
+++ wise-webapp/src/main/java/com/wisemapping/persistence/ImageSaver.java
@@ -1,6 +1,7 @@
 package com.wisemapping.persistence;
 
 import com.wisemapping.model.Image;
+import org.jetbrains.annotations.NotNull;
 
 import java.io.FileOutputStream;
 import java.io.IOException;
@@ -13,20 +14,23 @@ public class ImageSaver {
 
     private ImageSaver() {}
 
-    public static void save(Image image, byte[] data) throws NoSuchAlgorithmException, IOException {
-        String name = image.getName();
-        final StringBuilder fileName = new StringBuilder(IMAGE_FOLDER);
+    @NotNull
+    public static String save(@NotNull final Image image, final byte[] data) throws NoSuchAlgorithmException, IOException {
+        final String name = image.getName();
+        final StringBuilder fileNameBuilder = new StringBuilder(IMAGE_FOLDER);
         final MessageDigest md5 = MessageDigest.getInstance("MD5");
         final byte[] digest = md5.digest(data);
-        fileName.append("/").append(name).append("-").append(new String(digest));
+        fileNameBuilder.append("/").append(name).append("-").append(new String(digest));
+        final String fileName = fileNameBuilder.toString();
         FileOutputStream outputStream = null;
         try {
-            outputStream = new FileOutputStream(fileName.toString());
+            outputStream = new FileOutputStream(fileName);
             outputStream.write(data);
         } finally {
             if (outputStream != null) {
                 outputStream.close();
             }
         }
+        return fileName;
     }
 }
