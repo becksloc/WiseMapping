2e4f58cd90322c475d05c3ff2f67f032b6fcbc38
Ezequiel Bergamaschi <ezequielbergamaschi@gmail.com>
2015-05-03 18:56:46 -0300

    only svg2

--- wise-webapp/src/test/java/com/wisemapping/test/export/ExportSVGBasedTest.java
+++ wise-webapp/src/test/java/com/wisemapping/test/export/ExportSVGBasedTest.java
@@ -25,7 +25,7 @@ public class ExportSVGBasedTest {
         exportSvg(svgFile, svgExpFile, svgXml);
 
     }
-
+    @Test(dataProvider = "Data-Provider-Function", enabled = false)
     public void exportPdfTest(@NotNull final File svgFile) throws IOException, ExportException, TranscoderException, ParserConfigurationException {
         final String name = svgFile.getName();
 
@@ -34,7 +34,7 @@ public class ExportSVGBasedTest {
         exportPdf(svgFile, pdfFile, svgXml);
 
     }
-
+    @Test(dataProvider = "Data-Provider-Function", enabled = false)
     public void exportPngTest(@NotNull final File svgFile) throws IOException, ExportException, TranscoderException, ParserConfigurationException {
         final String name = svgFile.getName();
 
